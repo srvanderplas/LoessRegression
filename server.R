71b35310c8d2365dcc065d6542d5947ba4b092a8
Susan VanderPlas (root) <srvanderplas@gmail.com>
2016-04-27 08:28:29 -0500

    Simple application to demonstrate loess regression

---
+++ server.R
@@ -0,0 +1,54 @@
+
+# This is the server logic for a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+library(shiny)
+library(magrittr)
+library(dplyr)
+library(lattice)
+library(ggplot2)
+library(tidyr)
+library(broom)
+library(gganimate)
+
+dataset <- data_frame(
+  x = seq(0, 10, .1) + rnorm(101, 0, .03),
+  y = .025 * (x - 5) ^ 3 + .125 * (x - 7) ^ 2 - .0825 * (x - 3) + rnorm(101, 0, 1)
+)
+
+shinyServer(function(input, output) {
+
+  output$loessPlot <- reactivePlot({
+    mod <- loess(
+      y ~ x, # formula
+      degree = input$degree, span = input$span,
+      data = dataset
+    )
+
+    fit <- augment(mod)
+
+    loess.data <- dataset %>%
+      mutate(dist = abs(x - input$center)) %>%
+      filter(rank(dist) / n() <= input$span) %>%
+      mutate(weight = (1 - (dist / max(dist)) ^ 3) ^ 3)
+
+    reg.formula <- ifelse(input$degree == 1, "y~x", "y~x*x") %>%
+      formula()
+
+    ggplot(loess.data, aes(x = x, y = y)) +
+      geom_point(aes(alpha = weight)) +
+      geom_smooth(aes(weight = weight, color = "Local\nregression"),
+                  method = "lm", formula = reg.formula, se = FALSE) +
+      geom_vline(aes(xintercept = input$center), lty = 2) +
+      geom_line(aes(y = .fitted, color = "LOESS"), data = fit) +
+      scale_color_manual("", values = c("LOESS" = "red", "Local\nregression" = "blue")) +
+      scale_alpha_continuous("Observation Weight") +
+      theme(legend.position = "bottom", legend.box = "horizontal") +
+      ggtitle("LOESS Regression")
+
+  })
+
+})
---
+++ ui.R
@@ -0,0 +1,49 @@
+
+# This is the user-interface definition of a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+library(shiny)
+
+shinyUI(fluidPage(
+
+  # Application title
+  titlePanel("Loess Regression Demonstration"),
+
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(
+    sidebarPanel(
+      radioButtons(
+        inputId = "degree",
+        label = "Local Regression Type",
+        choices = c("Linear" = 1,
+                    "Quadratic" = 2),
+        selected = 2,
+        inline = T
+      ),
+      sliderInput(
+        inputId = "span",
+        label = "Smoothing Window Size",
+        min = 0.1,
+        max = 1,
+        step = 0.05,
+        value = 0.75
+      ),
+      sliderInput(
+        inputId = "center",
+        label = "Smoothing Window Location",
+        min = 0,
+        max = 10,
+        step = 0.01,
+        value = 0
+      )
+    ),
+
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("loessPlot")
+    )
+  )
+))
